9dd3cd4392966db5ddf23f778747fb74966adfa3
Yury Lvov <yury.lvov@leverages.jp>
2019-12-03 11:46:08 +0900

    Initial Commit

---
+++ mapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapApp
+//
+//  Created by リヴォーフ　ユーリ on 2019/12/03.
+//  Copyright © 2019 リヴォーフ　ユーリ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
